04e37280691a14faef86b6631af5375473f56042
teyang-lau <ty_lau@outlook.com>
2022-10-11 16:25:54 +0800

    fixed raw format reading problem

--- scripts/extract_frames.py
+++ scripts/extract_frames.py
@@ -14,34 +14,47 @@ parser.add_argument("-i", "--ignore_prev_vid", type=bool, default=False, help="W
 
 args = vars(parser.parse_args())
 
-def video2frames( video_file, output_path, factor=1 ):
+def video2frames(video_file, output_path, factor=1, youtube=False):
 
-    """Extract frames from a video file
+    """Extract frames from a video file or youtube link
     
     Args:
     video_file (str): path to the video
     output_path (str): path to output folder for storing extracted frames
     factor (int): how many seconds to extract 1 frame. 1 = extract a frame every sec, 2 = extract a frame every 2 secs
+    youtube (bool): whether to get video directly from youtube link
 
     """
-
     if not os.path.exists(output_path):
         os.makedirs(output_path)
-    vid = cv2.VideoCapture(video_file)
+
+    if youtube == False:
+        vid = cv2.VideoCapture(video_file)
+    
+    elif youtube == True:
+        video = pafy.new(video_file)
+        best = video.getbest(preftype="mp4")
+        vid = cv2.VideoCapture(best.url)
+    
     fps = round(vid.get(cv2.CAP_PROP_FPS))
+    num_frames = int(vid. get(cv2. CAP_PROP_FRAME_COUNT))
     index = 0        
     while vid.isOpened():
         success, img = vid.read()
+        index += 1
         if success:
-            index += 1
             # extract every fps frame of the video, multplied by a factor
             # factor of 1 = extract a frame every sec, 2 = extract a frame every 2 secs
             if index % (fps*factor) == 0:
                 cv2.imwrite(output_path + '/' + str(uuid.uuid4()) + '_' + str(index) + '.jpg', img)
-        else:
+        # stop reading at end of video
+        # need this as some frames return False success, so cannot
+        # use success to break the while loop
+        if index > num_frames:
             break
     vid.release()
-    return
+    
+    return 
 
 def multiple_video2frames( video_path, output_path , ignore_prev_vid=False, prev_vid_file=None ):
 
