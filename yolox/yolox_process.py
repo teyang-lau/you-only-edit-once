e2270e02d94a9d1051f2b1d543e1918d7bd8e092
teyang-lau <ty_lau@outlook.com>
2022-10-20 23:50:26 +0800

    import from gathered scripts

--- yolox/yolox_process.py
+++ yolox/yolox_process.py
@@ -4,10 +4,11 @@ import time
 import cv2
 import torch
 
-from data.data_augment import ValTransform
-from data.datasets import YOEO_CLASSES
-from exp import get_exp
-from utils import postprocess, vis
+from data_augment import ValTransform
+from yoeo_classes import YOEO_CLASSES
+from build import get_exp
+from visualize import vis
+from boxes import postprocess
 
 
 class Predictor(object):
