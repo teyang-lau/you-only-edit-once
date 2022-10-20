c8e020d36d952806951933d12a7ffcedae1c0efc
teyang-lau <ty_lau@outlook.com>
2022-10-20 22:54:29 +0800

    compute factors from fps

--- src/utils/streamlit.py
+++ src/utils/streamlit.py
@@ -4,6 +4,7 @@ Streamlit utils
 
 import os
 import streamlit as st
+from functools import reduce
 
 
 def save_uploaded_file(uploadedfile, tempDir):
@@ -15,3 +16,12 @@ def save_uploaded_file(uploadedfile, tempDir):
     st.success("Saved file :{} in tempDir".format(uploadedfile.name))
 
     return os.path.join(orig_video_path, uploadedfile.name)
+
+
+def factors(n):
+    return set(
+        reduce(
+            list.__add__,
+            ([i, n // i] for i in range(1, int(n**0.5) + 1) if n % i == 0),
+        )
+    )
