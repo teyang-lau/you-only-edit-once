614ed720c7455df1e1bb54e5f1e503a962b4366b
teyang-lau <ty_lau@outlook.com>
2022-10-22 13:07:03 +0800

    added make grid

--- src/utils/streamlit.py
+++ src/utils/streamlit.py
@@ -38,3 +38,11 @@ def get_all_files(path):
             filelist.append(os.path.join(root, file))
 
     return filelist
+
+
+def make_grid(cols, rows):
+    grid = [0] * cols
+    for i in range(cols):
+        with st.container():
+            grid[i] = st.columns(rows)
+    return grid
