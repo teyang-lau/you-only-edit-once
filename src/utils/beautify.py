5e8ff2a1e3ffdeae97325e078e963ceadaeb110f
tituslhy <tituslhy@gmail.com>
2022-10-22 18:08:57 +0800

    Update beautify.py

--- src/utils/beautify.py
+++ src/utils/beautify.py
@@ -104,7 +104,6 @@ def brightness(im_file):
   Returns perceived brightness of image
   https://www.nbdtech.com/Blog/archive/2008/04/27/Calculating-the-Perceived-Brightness-of-a-Color.aspx
   '''
-  im = Image.open(im_file)
   stat = ImageStat.Stat(im)
   r,g,b = stat.mean
   return math.sqrt(0.241*(r**2) + 0.691*(g**2) + 0.068*(b**2))
